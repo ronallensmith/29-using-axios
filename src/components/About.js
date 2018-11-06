7656b9c9f9c13eb09642f497659aba0eca06a821
ronallensmith <ron@ronallensmith.com>
2018-11-06 07:22:58 -0800

    first commit to GitHub

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About Page</h4>
+      <p>About! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel explicabo, architecto delectus obcaecati cum soluta accusamus est dolorem maiores asperiores tempore repellat ea ratione modi voluptatum perspiciatis ut. Ipsam, vitae!</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact Page</h4>
+      <p>Contact! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel explicabo, architecto delectus obcaecati cum soluta accusamus est dolorem maiores asperiores tempore repellat ea ratione modi voluptatum perspiciatis ut. Ipsam, vitae!</p>
+    </div>
+  )
+}
+
+export default Contact;
